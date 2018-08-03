f6d1e98e901266599a87138f4537333b1a2c0a41
Dejan Stjepanovic <ds2q88@gmail.com>
2018-08-03 15:40:01 -0500

    practice

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let button = document.getElementById("submit-btn");
+let button = document.querySelector(".submit-btn");
 
 
 //when the button is clicked, console log
@@ -9,27 +9,32 @@ button.addEventListener("click", function(){
 
 //print the junior instructors to the DOM
 let jrInstructors = document.getElementById("jr-instructors")
-staff.forEach(jrInstructor => {
-    jrInstructors.appendChild(`${jrInstructor} is a junior instructor at NSS`);
+staff.juniorInstructors.forEach(jrInstructor => {
+    let element = document.createElement("p")
+    element.textContent = `${jrInstructor} is a junior instructor at NSS`;
+    jrInstructors.appendChild(element)
 });
 
 
 //console log each of the schools in the school array individually and console log them
-schools.forEach(schools => {
-    console.log(schools);
-});
+
+for(let i = 0; i < schools.length; i++) {
+    console.log(schools[i]);
+};
 
 
 //print each of the lead instructors to the console
-for(let i = 0; i <= staff.instructors.length; i++){
-    console.log(staff.instructors[i]);
+for(let i = 0; i < staff.instructors.length; i++){
+    console.log("lead instructors", staff.instructors[i]);
 }
 
 
 //print the result to the console
 let parkInfo = {};
 fetch("https://external-data-56670.firebaseio.com/park-info.json")
-.then(result => {
+.then(res => res.json())
+.then((result) => {
     parkInfo = result;
+    parkInfo
+    console.log(parkInfo);
 });
-console.log(parkInfo);
